466d83061894f95cb9c3b5879783d248ee7a21db
Jeremy <jezzay@users.noreply.github.com>
2017-06-18 20:14:02 +1000

    Responsive updates  - include popular tracks in a modal

--- lastfm-fe/src/ArtistProfile.js
+++ lastfm-fe/src/ArtistProfile.js
@@ -14,7 +14,11 @@ class ArtistProfile extends Component {
 
     fetchArtistTopTracks() {
         Api.artistTopTracks(this.props.mbid, this.state.pageNumber).then((res) => {
-            this.setState({results: res.data, statusMsg: ''});
+            this.setState({results: res.data, statusMsg: ''}, () => {
+                const $ = window.$;
+                const id = "#artistProfileModal" + this.props.mbid;
+                $(id).modal()
+            });
         }, (err) => {
             err.response.then((reason) => {
                 console.log(`Failed because of ${reason.error}`);
@@ -53,13 +57,36 @@ class ArtistProfile extends Component {
                 <div className="col-xs-3 col-sm-1">
                     {this.props.name}
                 </div>
-                <div className="col-xs-8">
-                    {topTracks}
-                    <PaginationButtons
-                        includeNextButton={this.state.results.length > 0}
-                        includePreviousButton={this.state.pageNumber > 1}
-                        onNext={this.onNext}
-                        onPrevious={this.onPrevious}/>
+                <div className="modal fade"
+                     id={"artistProfileModal" + this.props.mbid}
+                     tabIndex="-1"
+                     role="dialog"
+                     aria-labelledby={"artistProfile" + this.props.mbid}>
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                <h4 className="modal-title"
+                                    id={"artistProfileLabel" + this.props.mbid}>{this.props.name} Top Tracks</h4>
+                            </div>
+                            <div className="modal-body">
+                                <div className="col-xs-12">
+                                    {topTracks}
+                                    <PaginationButtons
+                                        includeNextButton={this.state.results.length > 0}
+                                        includePreviousButton={this.state.pageNumber > 1}
+                                        onNext={this.onNext}
+                                        onPrevious={this.onPrevious}/>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-default" data-dismiss="modal">Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
